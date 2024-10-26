a3b9601a155fac9d332ab4aa8f86e4d63d1649d3
Satish Rathod <satishrathod.dev@gmail.com>
2024-10-26 22:41:36 +0530

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import Navbar from "./Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Contact from "./Contact";
+import About from "./About";
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="App">
+      {/* Do not remove the main div */}
+      Hey there!!
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
